744a981f8ee44566a59c3f1c25364f792bbc7e95
Isaac C <isaac.c@tutanota.com>
2022-08-30 14:08:52 -0700

    Finished adding TIMER1 for audio driver to control note length

--- drivers/audio_driver/audio_driver.c
+++ drivers/audio_driver/audio_driver.c
@@ -1,20 +1,35 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "audio_driver.h"
 
 /**
  * init_audio() - initialize audio settings
  *
- * Set up the TIMER0 in CTC mode to continuously generate a square wave at a
- * frequency determined by note in the song.
+ * Set up TIMER0 and TIMER1, which will be used to generate audio.
+ * Note that TIMER1 may also be used to control the pace of the tetris game,
+ * this will be noted in the ISR of TIMER1.
  *
  * Return: void
  */
 void init_audio(unsigned char starting_note)
 {
+    // Initialize 8-bit TIMER0, which will generate the sqr wave to produce note
     DDRB |= (1 << DDB3); //Output on PB3
-    TCCR0 = (1 << WGM01)|(1<<COM00); //|(1 << CS02);
-    OCR0 = starting_note;
+    TCCR0 = (1 << WGM01)|(1<<COM00);
+    OCR0  = starting_note;
     TCNT0 = 0;
+
+    // Initialize 16-bit TIMER1, which will be a 250 ms timer to control the
+    // length of each note amongst other things. See the ISR definition.
+    TCCR1A = 0;
+    TCCR1B = (1 << WGM12)|(1 << CS12)|(1 << CS10);
+    OCR1A = 3905; // 250 ms
+    TCNT1 = 0;
+    // Enable the interrupt for TIMER1
+#warning probably better placement for these rather than in init_audio()
+    sei();
+    TIMSK = (1 << OCIE1A); // enable interrupt
+    TIFR  = (1 << OCF1A);  // allow execution of interrupt
 }
 
 /**
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/eeprom.h>
+#include <avr/interrupt.h>
 #include "display_driver.h"
 #include "spi_driver.h"
 #include "audio_driver.h"
@@ -7,20 +8,28 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
+ISR(TIMER1_COMPA_vect)
+{
+    // toggle led here
+    PORTC ^= (1 << 0);
+}
+
 int main(void)
 {
     display.init();
     audio.init(NOTE_E4);
-    audio.play();
-    _delay_ms(500);
-    audio.change_note(NOTE_A4);
-    _delay_ms(500);
-    audio.stop();
+    /* audio.play(); */
+    /* _delay_ms(500); */
+    /* audio.change_note(NOTE_A4); */
+    /* _delay_ms(500); */
+    /* audio.stop(); */
+
     /* display.spi_send_cmd(OP_DIGIT2, 0xFF); */
     /* display.spi_send_cmd(OP_DIGIT3, 0xFF); */
-
+    DDRC |= (1<<0);
     for(;;) {
-    }
+
+	}
 }
 
 //eeprom_update_byte(0, 0xEE);
